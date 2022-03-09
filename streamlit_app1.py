73e27239d0d032a135aa0832162a8034ec7fc847
girdharkolan <56930069+girdharkolan@users.noreply.github.com>
2022-03-09 17:07:49 +0400

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -21,28 +21,19 @@ dataframe = pd.DataFrame(
 st.dataframe(dataframe.style.highlight_max(axis=0))
 
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 5),
-    columns=('col %d' % i for i in range(5)))
-st.table(dataframe)
-
-
+# Draw a line chart
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
-
 st.line_chart(chart_data)
 
-
-
+# Draw a Map
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
-
 st.map(map_data)
-
 
 
 import streamlit as st
 x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+st.write(x, 'Qubed is', x * x * x)
